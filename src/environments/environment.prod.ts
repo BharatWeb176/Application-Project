ca6af4a0e0199d6d2acb1d0857144bd07cb94eac
Bharat Jangir <jangirbharat.ca@gmail.com>
2025-02-27 18:04:18 -0500

    bug fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'happypaws-621b1',
-    appId: '1:957404999094:web:38ded4e2bb534d0ace2517',
-    storageBucket: 'happypaws-621b1.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyChst9Uo1po9_nMuuK4rN21r5MRbfZlRus',
-    authDomain: 'happypaws-621b1.firebaseapp.com',
-    messagingSenderId: '957404999094',
+    apiKey: 'AIzaSyDCxE3fW1Mb3blY2LQTRqD45gax5I3a1dE',
+    authDomain: 'pet-care-e53f1.firebaseapp.com',
+    projectId: 'pet-care-e53f1',
+    storageBucket: 'pet-care-e53f1.firebasestorage.app',
+    messagingSenderId: '312408503771',
+    appId: '1:312408503771:web:c1b765ac169de1a7aaa3fe',
+    measurementId: 'G-D2BLFCTY0X',
   },
-  production: true
+  production: true,
 };
